47c26292ce3a1cbf8fa2e040c3e057bc4ac86004
Rajib Ahmed <rajib.ahmed@klarna.com>
2020-04-18 02:06:29 +0200

    pulls songs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+declare global {
+  interface Window {
+    SC: {
+      get: (path: string, options?: {}) => Promise<any>;
+    };
+  }
+}
+
+const App = () => {
+  const [song, setSong] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    window.SC.get("/tracks", { q: song }).then((songsResponse) =>
+      setResults(songsResponse)
+    );
+  }, [song]);
+
   return (
-    <div className="App">
+    <div className="amz-app">
+      <h1>this is a app</h1>
+      <input
+        name="song"
+        onChange={(e) => e.target.value && setSong(e.target.value)}
+      />
+      {song && <h3>{song}</h3>}
+      {results &&
+        results.map((r: { created_at: string }) => <p>{r.created_at}</p>)}
     </div>
   );
-}
+};
 
 export default App;
